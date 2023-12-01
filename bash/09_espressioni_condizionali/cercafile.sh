1e7ad32e41f4a626f1c0e9fdd6b864b4ca8b9623
Luca Marchi <luca.marchi13@studio.unibo.it>
2023-12-01 17:22:54 +0100

    renamed a folder

--- bash/09_espressioni_condizionali/cercafile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for letter in {c..g}; do
-      for name in /usr/include/?${letter}*; do
-            if [[ -e ${name} && (${#name} -lt 18 || ${#name} -gt 23) ]]; then
-                echo "${name}"
-            fi
-      done
-done 
-
